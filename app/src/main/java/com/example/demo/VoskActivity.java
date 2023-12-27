a3f93c30dc994f69da8af1b89bd978d65c94a832
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2023-12-27 22:15:14 +0530

    ChatBot Text Mode Complete

--- app/src/main/java/com/example/demo/VoskActivity.java
+++ app/src/main/java/com/example/demo/VoskActivity.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.vosk.demo;
+package com.example.demo;
 
 import android.Manifest;
 import android.app.Activity;
